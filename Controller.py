2ee4bb922186905b9c1fc5b3cb35c480663fde49
EndProgramm <62557797+endprogramm@users.noreply.github.com>
2020-05-26 10:24:31 +0200

    Seperate - seperated main.py into the Main classes Controller, View and Model    - moved parts that didn't belong to one Class in terms of the MVC-Scheme to the correct one - created new function to create a menubar in preparation for better use of OOP

---
+++ Controller.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+# Autoren: Martin, Max, Vincent, Christoph, Lia; erstellt: 24.02.2020
+# Der Projektwochenmanager zum zuordnen aller Schüler zu Projekten
+
+import os, time
+from Model import Model
+from View import View
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter import filedialog  # muss aus unbekannten Gründen extra importiert werden
+
+
+class Controller(object):
+    def __init__(self):
+        self.model = Model()
+        self.view = View(self.importieren, self.exportieren, self.beenden, self.J5, self.J6, self.J7, self.J8, self.J9,
+                         self.J10, self.J11, self.J12, self.tabelle_update, self.tabelle_sorti, self.hinzufugen,
+                         self.zuordnen, self.ande, self.a1, self.a2, self.a3, self.a4, self.a5, self.a6, self.a7)
+        self.wahlen = ('sErst', 'sZweit', 'sDritt')
+        self.delimiter = {'imp_s': None, 'imp_p': None, 'exp': None}
+        self.dchosen = None
+        self.slcsv = 'schuelerliste.csv'
+        self.plcsv = 'projektliste.csv'
+        self.double = False
+        self.andernx = ""
+
+        self.tabelle()
+        self.view.table.bind('<Double-Button-1>', self.treevent)
+        # Erstimportierung
+        if self.model.ausgabe('schueler'):
+            self.importieren(True)
+
+        self.view.mainloop()
+
+    def treevent(self, event):
+        if self.view.table.identify_region(event.x, event.y) == 'cell':
+            self.view.schuelerandern()
+            self.view.entrys['ande_Eid'].insert(0, event.widget.selection()[0])
+
+    def zuordnen(self):
+        if not self.double:
+            self.double = True
+            try:
+                self.view.fenster['popup'].destroy()
+            except (TclError, KeyError):
+                pass
+            self.view.popup_Progressbar()
+            self.view.fenster['popup'].update()
+            for wahl in self.wahlen:
+                self.model.zuordnen(wahl)
+                self.view.labels['popup'].step(30)
+                self.view.fenster['popup'].update()
+            erg = self.model.restzuordnung()
+            if erg[0]:
+                showwarning('Fehler', 'In dem Jahrgang/den Jahrgängen ' + str(
+                    erg[1]) + ' gibt es mehr Schüler als Plätze in den Kursen.')
+            self.view.labels['popup'].step(10)
+            self.view.fenster['popup'].update()
+            time.sleep(0.5)
+            self.tabelle_update()
+            self.view.fenster['popup'].destroy()
+            self.double = False
+        else:
+            showwarning('Doppelte Operation',
+                        'Es wird bereits eine Zuordnung durchgeführt, bitte warten Sie bis zur Vollendung dieser, bis Sie die nächste ausführen')
+
+    def delimOK(self):
+        if isinstance(self.dchosen, tuple):
+            for d in self.dchosen:
+                self.delimiter[d] = self.view.entrys['popup'].get()
+        else:
+            self.delimiter[self.dchosen] = self.view.entrys['popup'].get()
+        if self.delimiter == '' or len(self.delimiter[self.dchosen]) != 1:
+            showwarning('Angabe ungültig', 'Das angegebene Trennzeichen ist ungültig oder es wurde keines Angegeben!'
+                                           '\nBitte Geben Sie ein anderes Trennzeichen ein!')
+            self.delimiterFester(None)
+        else:
+            self.view.fenster['popup'].destroy()
+            self.dchosen = None
+
+    def delimCanc(self):
+        self.view.fenster['popup'].destroy()
+
+    def delimiterFester(self, d):
+        if isinstance(d, tuple):
+            self.dchosen = list(self.delimiter.keys())[d[0]], list(self.delimiter.keys())[d[1]]
+        elif self.dchosen is None:
+            self.dchosen = list(self.delimiter.keys())[d]
+        if d == 0:
+            self.view.popup_textentry('Bitte geben Sie das Trennzeichen der Schuelerlisten CSV-Datei an:', self.delimOK,
+                                      self.delimCanc)
+        if d == 1:
+            self.view.popup_textentry('Bitte geben Sie das Trennzeichen der Projektlisten CSV-Datei an:', self.delimOK,
+                                      self.delimCanc)
+        if d == 2:
+            self.view.popup_textentry('Bitte geben Sie das Trennzeichen der CSV-Dateien an:', self.delimOK,
+                                      self.delimCanc)
+        while self.dchosen is not None:
+            try:
+                self.view.fenster['popup'].update()
+            except TclError:
+                return False
+        return True
+
+    def autoimport(self):
+        if os.path.exists('projektliste.csv') and os.path.exists('schuelerliste.csv') and askokcancel('Auto-import',
+                                                                                                      'Es wurden passende CSV-Dateien gefunden, wollen Sie diese jetzt importieren?'):
+            self.slcsv = 'schuelerliste.csv'
+            self.plcsv = 'projektliste.csv'
+        else:
+            self.slcsv = ''
+            self.plcsv = ''
+
+    def csvimport(self):
+        if self.plcsv != "" and self.delimiterFester(0) and self.delimiterFester(1) and \
+                self.model.importCSV(self.slcsv, self.plcsv, self.delimiter['imp_s'], self.delimiter['imp_p']):
+            showwarning('Fehler', 'Falscher Delimiter oder falsches Tabellenformat')
+            self.csvimport()
+        else:
+            showinfo('Importiert', 'Die CSV Dateien wurden importiert!')
+
+    def importieren(self, erstes=False):
+        if not bool(self.model.ausgabe('schueler')):
+            self.autoimport()
+
+        elif not erstes and askyesno('Bereits importiert', 'Es sind bereits CSV-Dateien importiert, wollen sie diese '
+                                                           'überschreiben?'):
+            self.model.clearDB()
+            self.model.createDB()
+            self.autoimport()
+        else:
+            return
+        if self.slcsv == "":
+            self.slcsv = filedialog.askopenfilename(title="Schülerliste importieren",
+                                                    filetypes=(("CSV Datei", "*.csv"), ("all files", "*.*")))
+            if self.slcsv != "":
+                self.plcsv = filedialog.askopenfilename(title="Projektliste importieren",
+                                                        filetypes=(("CSV Datei", "*.csv"), ("all files", "*.*")))
+        self.csvimport()
+
+        if not bool(self.view.table.get_children()):
+            self.tabelle()
+        else:
+            self.tabelle_update()
+
+    def exportieren(self):
+        slcsv = filedialog.asksaveasfilename(title='Schülerliste exportieren', defaultextension=".csv",
+                                             initialfile='schuelerl_fertig',
+                                             filetypes=(("CSV Datei", "*.csv"), ("Txt Datei", "*.txt"),
+                                                        ("all files", "*.*")))
+        if slcsv is not None:
+            plcsv = filedialog.asksaveasfilename(title='Projektliste exportieren', defaultextension=".csv",
+                                                 initialfile='projektel_fertig',
+                                                 filetypes=(("CSV Datei", "*.csv"), ("Txt Datei", "*.txt"),
+                                                            ("all files", "*.*")))
+            if plcsv is not None:
+                if self.delimiterFester(2):
+                    self.model.exportCSV(slcsv, plcsv, self.delimiter['exp'])
+                    showinfo('Exportiert', "Die Tabellen wurden in zwei CSV-Dateien mit '" + self.delimiter['exp'] +
+                             "' als Trennzeichen ausgegeben!")
+
+    def beenden(self):
+        x = askokcancel(title='Beenden',
+                        message='Möchtest du das Programm wirklich beenden?'
+                                '\nNicht abgeschlossene Aktionen könnten zu fehlern führen!')
+        if x:
+            self.view.destroy()
+
+    def tabelle(self, fetch=None):
+        if fetch is None:
+            fetch = self.model.ausgabe('schueler')
+        self.view.tabelle(fetch)
+
+    def tabelle_update(self, fetch=None):
+        try:
+            self.view.table.delete(*self.view.table.get_children())
+            if fetch is None:
+                fetch = self.model.ausgabe('schueler')
+            for t in fetch:
+                self.view.table.insert('', t[0], t[0], values=t)
+        except AttributeError:
+            pass
+
+    def tabelle_sorti(self, col, descending):
+        data = [(self.view.table.set(tvindex, col), tvindex) for tvindex in self.view.table.get_children('')]
+
+        try:
+            data = [(float(element), tvindex) for element, tvindex in data]
+        except ValueError:
+            pass
+
+        data.sort(reverse=descending)
+
+        for index, (val, k) in enumerate(data):
+            self.view.table.move(k, '', index)
+
+        # reverse sort next time
+        self.view.table.heading(col, command=lambda: self.tabelle_sorti(col, not descending))
+
+    def hinzufugen(self):
+        for entry in range(7):
+            self.view.entrys[entry].config(background='white')
+        erst = self.view.entrys[4].get()
+        zweit = self.view.entrys[5].get()
+        dritt = self.view.entrys[6].get()
+        if dritt == "":
+            dritt = "33"
+        if zweit == "":
+            zweit = "33"
+        if erst == "":
+            erst = "33"
+        if self.view.entrys[0].get() != "" and self.view.entrys[1].get() != "" and \
+                self.view.entrys[2].get() != "" and self.view.entrys[3].get() != "":
+            if self.model.einfuegen('schueler', ('sName', 'sVName', 'sJahrg', 'sKla', 'sErst', 'sZweit', 'sDritt'),
+                                    (self.view.entrys[1].get(), self.view.entrys[0].get(), self.view.entrys[2].get(),
+                                     self.view.entrys[3].get(), erst, zweit, dritt)):
+                for entry in range(7):
+                    self.view.entrys[entry].delete(0, END)
+                self.view.buttons['hin'].configure('confirm')
+            else:
+                showerror('Fehler', 'Schüler bereits vorhanden!')
+            for entry in range(7):
+                self.view.entrys[entry].delete(0, END)
+                self.view.entrys[entry].configure('default')
+        else:
+            self.view.buttons['hin'].configure('error')
+            for i in range(4):
+                self.view.entrys[i].configure('default')
+                if self.view.entrys[i].get() == "":
+                    self.view.entrys[i].configure('error')
+        self.view.update()
+        time.sleep(0.3)
+        self.view.buttons['hin'].configure('default')
+        self.tabelle_update()
+
+    def ande(self):
+        if self.andernx != "" and self.view.entrys['ande_Eid'].get() != "":
+            self.model.ande(self.andernx, self.view.entrys['ande_Eandern'].get(), self.view.entrys['ande_Eid'].get())
+            self.tabelle_update()
+            self.view.fenster['ande'].destroy()
+
+    def J5(self):
+        self.tabelle_update(self.model.jahrgsuch(5))
+
+    def J6(self):
+        self.tabelle_update(self.model.jahrgsuch(6))
+
+    def J7(self):
+        self.tabelle_update(self.model.jahrgsuch(7))
+
+    def J8(self):
+        self.tabelle_update(self.model.jahrgsuch(8))
+
+    def J9(self):
+        self.tabelle_update(self.model.jahrgsuch(9))
+
+    def J10(self):
+        self.tabelle_update(self.model.jahrgsuch(10))
+
+    def J11(self):
+        self.tabelle_update(self.model.jahrgsuch(11))
+
+    def J12(self):
+        self.tabelle_update(self.model.jahrgsuch(12))
+
+    def a1(self):
+        self.andernx = "sVName"
+
+    def a2(self):
+        self.andernx = "sName"
+
+    def a3(self):
+        self.andernx = "sJahrg"
+
+    def a4(self):
+        self.andernx = "sKla"
+
+    def a5(self):
+        self.andernx = "sErst"
+
+    def a6(self):
+        self.andernx = "sZweit"
+
+    def a7(self):
+        self.andernx = "sDritt"
+
+    def unreif(self):
+        showwarning('Noch nicht ausgereift', 'Dieser Teil wurde noch nicht Programmiert')
+
+
+c = Controller()
---
+++ View.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+# Autoren: Martin, Max, Vincent, Christoph, Lia; erstellt: 24.02.2020
+# Der Projektwochenmanager zum zuordnen aller Schüler zu Projekten
+
+from tkinter import *
+from tkinter import filedialog  # muss aus unbekannten Gründen extra importiert werden
+from tkinter.messagebox import *
+from tkinter.ttk import *
+import ttkthemes
+
+
+class View(ttkthemes.ThemedTk):
+    def __init__(self, imp, exp, bee, j5, j6, j7, j8, j9, j10, j11, j12, ja, tabsort, hin, zord, ande, a1, a2, a3, a4,
+                 a5, a6, a7):
+        # print(ttkthemes.THEMES)   # zum Ausgeben der verfügbaren Themes
+        ttkthemes.ThemedTk.__init__(self, theme='breeze')
+        self.title("Projektwochenverwaltungsprogramm")
+        self.geometry('800x300')
+        self.maxsize(800, 300)
+        self.minsize(800, 300)
+
+        # Ttk Styles
+        self.styles = Style().configure('confirm', background='green')
+        self.styles = Style().configure('error', background='red')
+        self.styles = Style().configure('default', background='SystemButtonFace')
+
+        # bestimmen der Callbacks
+        self.callback_imp = imp
+        self.callback_exp = exp
+        self.callback_bee = bee
+        self.radiocom = {'jahrg': [j5, j6, j7, j8, j9, j10, j11, j12, ja], 'ande': [a1, a2, a3, a4, a5, a6, a7]}
+        self.tabelle_sorti = tabsort
+        self.callback_zord = zord
+        self.callback_hin = hin
+        self.callback_ande = ande
+        self.names = {'schueler': ("Vorname", "Nachname", "Jahrg", "Klasse", "Erst Wunsch", "Zweit Wunsch",
+                                   "Dritt Wunsch"),
+                      'jahrg': ('5', '6', '7', '8', '9', '10', '11', '12', 'Alle')}
+        self.labels = {}
+        self.entrys = {}
+        self.buttons = {}
+        self.vars = {'jahrg': IntVar(), 'ande': IntVar()}
+        self.radios = {}
+        self.rahmen = {1: Frame(master=self), 2: Frame(master=self)}
+        self.rahmen.update({11: Frame(master=self.rahmen[1])})
+        self.fenster = {}
+
+        # erstellen des Menüs
+        self.menus = {}
+        self.menu((('Datei', (
+            ("importieren", self.callback_imp), ("exportieren", self.callback_exp), ("-",),
+            ("beenden", self.callback_bee))),
+                   ('Schüler', (("hinzufügen", self.schulerhin), ("ändern", self.schuelerandern))),
+                   ('Tools', (("Schüler Projekten zuordnen", self.callback_zord),))))
+        for i in range(len(self.names['jahrg'])):
+            self.radios['jahrg-' + self.names['jahrg'][i]] = Radiobutton(self.rahmen[11], text=self.names['jahrg'][i],
+                                                                         variable=self.vars['jahrg'], value=i,
+                                                                         command=self.radiocom['jahrg'][i])
+            self.radios['jahrg-' + self.names['jahrg'][i]].pack(side=LEFT)
+        self.radios['jahrg-Alle'].invoke()
+
+        # Tabelle
+        self.scrollbar = Scrollbar(self.rahmen[2], orient="vertical")
+        self.table = Treeview(self.rahmen[2], yscrollcommand=self.scrollbar.set)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.rahmen[1].pack()
+        self.rahmen[2].pack(fill=X)
+        self.rahmen[11].pack(side=LEFT, fill=X)
+
+    def menu(self, menu):
+        self.menus = {'main': Menu(self)}
+        for cas, com in menu:
+            self.menus[cas] = Menu(self.menus['main'], tearoff=0)
+            print(com)
+            for command in com:
+                print(command)
+                if command[0] == '-':
+                    self.menus[cas].add_separator()
+                else:
+                    self.menus[cas].add_command(label=command[0], command=command[1])
+            self.menus['main'].add_cascade(label=cas, menu=self.menus[cas])
+        self.config(menu=self.menus['main'])
+
+    def tabelle(self, fetch):
+        width = [35, 75, 75, 50, 50, 100, 100, 100, 75]
+        ml = ['ID']
+        for namen in self.names['schueler']:
+            ml.append(namen)
+        ml.append('Zugeordned zu')
+        self.table['columns'] = ml
+        self.table['show'] = 'headings'
+        for i in range(len(ml)):
+            self.table.column(ml[i], width=width[i], minwidth=width[i])
+        for i in range(len(ml)):
+            self.table.heading(ml[i], text=ml[i], command=lambda col=i: self.tabelle_sorti(col, False))
+        for t in fetch:
+            self.table.insert('', t[0], t[0], values=t)
+        self.scrollbar.config(command=self.table.yview)
+        self.table.pack(fill=X)
+
+    def popup_textentry(self, text, call_ok, call_cancel):
+        self.fenster['popup'] = Tk()
+        self.fenster['popup'].title('Aktion erforderlich!')
+        self.fenster['popup'].geometry("300x125")
+        self.fenster['popup'].resizable(0, 0)
+        self.labels['popup'] = Label(self.fenster['popup'], text=text, font=('Arial', 13), wraplength=275,
+                                     anchor='center')
+        self.entrys['popup'] = Entry(self.fenster['popup'])
+        self.rahmen['popup'] = Frame(self.fenster['popup'])
+        self.buttons['popup_OK'] = Button(self.rahmen['popup'], text='Ok', command=call_ok, width=10)
+        # self.buttons['popup_Cancel'] = Button(self.rahmen['popup'], text='Cancel', command=call_cancel, width=10)
+        self.labels['popup'].pack(fill=X)
+        self.entrys['popup'].pack(pady=15)
+        self.rahmen['popup'].pack()
+        self.buttons['popup_OK'].pack(side=LEFT)
+        # self.buttons['popup_Cancel'].pack(side=LEFT)
+
+    def popup_Progressbar(self):
+        self.fenster['popup'] = Tk()
+        self.fenster['popup'].title('In Bearbeitung!')
+        self.fenster['popup'].resizable(0, 0)
+        self.labels['popup'] = Progressbar(self.fenster['popup'], orient='horizontal', length=250, mode='determinate')
+        self.labels['popup'].pack()
+
+    def schulerhin(self):
+        self.fenster['hin'] = Tk()
+        self.fenster['hin'].title("Neue Schüler")
+        self.fenster['hin'].geometry('780x110')
+        for i in range(len(self.names['schueler'])):
+            self.labels[self.names['schueler'][i]] = Label(self.fenster['hin'], text=self.names['schueler'][i])
+            self.labels[self.names['schueler'][i]].place(x=10 + (110 * i), y=10, width=100)
+        for i in range(len(self.names['schueler'])):
+            self.entrys[i] = Entry(self.fenster['hin'])
+            self.entrys[i].place(x=10 + (110 * i), y=40, width=100)
+        self.buttons['hin'] = Button(self.fenster['hin'], text="Schüler hinzufügen", command=self.callback_hin)
+        self.buttons['hin'].place(x=330, y=70, width=120, height=30)
+
+    def schuelerandern(self):
+        try:
+            self.fenster['ande'].destroy()
+        except TclError:
+            pass
+        self.fenster.update({'ande': Tk()})
+        self.fenster['ande'].title("Schüler ändern")
+        self.fenster['ande'].geometry('725x110')
+
+        self.rahmen['ande'] = Frame(self.fenster['ande'])
+
+        self.rahmen['ande_ID'] = Frame(self.rahmen['ande'])
+        self.labels['ande_idLab'] = Label(self.rahmen['ande_ID'], text="ID")
+        self.labels['ande_idLab'].pack(fill=X, pady=2)
+        self.entrys['ande_Eid'] = Entry(self.rahmen['ande_ID'])
+        self.entrys['ande_Eid'].pack(side=LEFT)
+        self.rahmen['ande_ID'].pack(side=LEFT)
+
+        self.rahmen['ande_change'] = Frame(self.rahmen['ande'])
+
+        self.rahmen['ande_r'] = Frame(self.rahmen['ande_change'])
+        for i in range(len(self.names['schueler'])):
+            self.radios['ande-' + self.names['schueler'][i]] = Radiobutton(self.rahmen['ande_r'],
+                                                                           text=self.names['schueler'][i],
+                                                                           variable=self.vars['ande'], value=i,
+                                                                           command=self.radiocom['ande'][i])
+            self.radios['ande-' + self.names['schueler'][i]].pack(side=LEFT, fill=X)
+        self.radios['ande-Vorname'].invoke()
+        self.rahmen['ande_r'].pack(fill=X)
+
+        self.rahmen['ande_e'] = Frame(self.rahmen['ande_change'])
+        self.entrys['ande_Eandern'] = Entry(self.rahmen['ande_e'])
+        self.entrys['ande_Eandern'].pack(fill=X)
+        self.rahmen['ande_e'].pack(fill=X)
+        self.rahmen['ande_change'].pack(fill=X, padx=4, pady=2)
+
+        self.rahmen['ande'].pack(fill=X, padx=6, pady=6)
+
+        self.buttons['ande_B'] = Button(self.fenster['ande'], text="ändern", command=self.callback_ande)
+        self.buttons['ande_B'].pack(fill=X, padx=8, pady=6)
